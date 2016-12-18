efb607534dd0ae56c8db057b641e76b7c486cddd
Manas Chaudhari <chaudhari.manas@gmail.com>
2016-12-19 00:26:50 +0530

    Display list of events and error

--- ArchitectureSample/Classes/Modules/GithubEvents/View/GithubEventsViewController.swift
+++ ArchitectureSample/Classes/Modules/GithubEvents/View/GithubEventsViewController.swift
@@ -12,9 +12,16 @@ class GithubEventsViewController: UIViewController, GithubEventsViewInput {
 
     var output: GithubEventsViewOutput!
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    var events: [GithubEvent]?
+    
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         output.viewIsReady()
     }
 
@@ -24,8 +31,29 @@ class GithubEventsViewController: UIViewController, GithubEventsViewInput {
     }
     
     func showEvents(events: [GithubEvent]) {
+        self.events = events
+        tableView.reloadData()
+        
+        tableView.isHidden = false
+        errorLabel.isHidden = true
     }
     
     func showError() {
+        tableView.isHidden = true
+        errorLabel.isHidden = false
+    }
+}
+
+extension GithubEventsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Event Cell", for: indexPath)
+        if let event = events?[indexPath.row] {
+            cell.textLabel?.text = event.type
+        }
+        return cell
     }
 }
