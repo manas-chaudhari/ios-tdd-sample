3ffd63c6f674ce676fd20967031a1fa8a5215eed
Manas Chaudhari <chaudhari.manas@gmail.com>
2016-12-27 18:39:57 +0530

    Extract GithubEvent model to a separate file | Move services outside VIPER Modules

--- ArchitectureSample/Classes/Services/GithubService.swift
+++ ArchitectureSample/Classes/Services/GithubService.swift
@@ -12,30 +12,6 @@ import Result
 import ObjectMapper
 import Moya_ObjectMapper
 
-
-struct GithubEvent: Equatable {
-    var id: Int!
-    var type: String!
-        
-    public static func ==(lhs: GithubEvent, rhs: GithubEvent) -> Bool {
-        return lhs.id == rhs.id &&
-            lhs.type == rhs.type
-    }
-}
-
-extension GithubEvent: Mappable {
-    /// This function can be used to validate JSON prior to mapping. Return nil to cancel mapping at this point
-    public init?(map: Map) {
-    }
-    
-    mutating func mapping(map: Map) {
-        id <- (map["id"], JSONTransforms.stringToInt)
-        type <- map["type"]
-    }
-
-    
-}
-
 protocol GithubServiceType {
     func events(completion: @escaping (Result<[GithubEvent], Moya.Error>) -> ())
 }
---
+++ ArchitectureSample/Classes/Services/Models/GithubEvent.swift
@@ -0,0 +1,33 @@
+//
+//  GithubEvent.swift
+//  ArchitectureSample
+//
+//  Created by Manas Chaudhari on 12/27/16.
+//  Copyright © 2016 Manas Chaudhari. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+struct GithubEvent: Equatable {
+    var id: Int!
+    var type: String!
+    
+    public static func ==(lhs: GithubEvent, rhs: GithubEvent) -> Bool {
+        return lhs.id == rhs.id &&
+            lhs.type == rhs.type
+    }
+}
+
+extension GithubEvent: Mappable {
+    /// This function can be used to validate JSON prior to mapping. Return nil to cancel mapping at this point
+    public init?(map: Map) {
+    }
+    
+    mutating func mapping(map: Map) {
+        id <- (map["id"], JSONTransforms.stringToInt)
+        type <- map["type"]
+    }
+    
+    
+}
